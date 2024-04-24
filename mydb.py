5fde9de07130018a5bdc84d757a56bbe4300e1a2
Denish Awajo <awajodenish@gmail.com>
2024-04-24 21:42:07 +0300

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = 'localhost',
+    user = 'root',
+    passwd = '12345678'
+)
+cursorObject = database.cursor()
+
+cursorObject.execute("CREATE DATABASE awajos")
+
+print("all done")
